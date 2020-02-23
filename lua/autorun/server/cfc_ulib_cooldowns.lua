950d7e2c5e91518cd5d5e16e2a2e108d27073ab4
PLlally <11771225+PLally@users.noreply.github.com>
2020-02-23 16:10:55 -0500

    whitespace and styling  changes

--- lua/autorun/server/cfc_ulib_cooldowns.lua
+++ lua/autorun/server/cfc_ulib_cooldowns.lua
@@ -1,11 +1,11 @@
 
--- { amount = 4, decayRate = 1 }
--- using a commands adds to its counter if this counter is greater than `amount` you can no longer is the command
--- the 1 is subtracted from the counter every `decayRate` seconds
+-- { amount = int, decayRate = int }
+-- using a commands adds to its counter if this counter is greater than `amount` you can no longer run the command
+-- 1 is subtracted from the counter every `decayRate` seconds
 local ULIB_COOLDOWNS = {
-    ["ulx ragdoll"] = { amount = 4, decayRate = 1 },
-    ["default"] = { amount = 100, decayRate = 1 },
-    ["ulx armor"] = { amount = 4, decayRate = 5 },
+    ["default"]       = {amount=10, decayRate=2},
+    ["ulx ragdoll"]   = {amount=2, decayRate=5},
+    ["ulx unragdoll"] = {amount=2, decayRate=5},
 }
 
 
@@ -13,11 +13,11 @@ local ULIB_COOLDOWNS = {
 local playerCooldowns = {}
 
 local function maxAmount( commandName )
-    return (ULIB_COOLDOWNS[commandName] or ULIB_COOLDOWNS.default).amount
+    return ( ULIB_COOLDOWNS[commandName] or ULIB_COOLDOWNS.default ).amount
 end
 
 local function decayRate( commandName )
-    return (ULIB_COOLDOWNS[commandName] or ULIB_COOLDOWNS.default).decayRate
+    return ( ULIB_COOLDOWNS[commandName] or ULIB_COOLDOWNS.default ).decayRate
 end
 
 local function cooldownExists( commandName ) 
@@ -54,10 +54,9 @@ local function canRun( ply, commandName )
     local cooldown = getCooldownTable( ply, commandName )
 
     local timeSince = CurTime() - cooldown.lastRun 
-    cooldown.count = cooldown.count - math.floor(timeSince/decayRate(commandName))
-    cooldown.count = math.max(0, cooldown.count)
+    cooldown.count = cooldown.count - math.floor( timeSince / decayRate( commandName ) )
+    cooldown.count = math.max( 0, cooldown.count )
 
-    print(cooldown.count, maxAmount( commandName ))
     if cooldown.count > maxAmount( commandName ) then
         return false
     end
@@ -74,12 +73,10 @@ local function newUsage( ply, commandName )
     cooldown.count = cooldown.count + 1
 end
 
-hook.Add( "ULibPostTranslatedCommand", "CFC_UlibCooldowns_PostTranslate")
 hook.Add( "ULibPostTranslatedCommand", "CFC_UlibCooldowns_PostTranslate", function( ply, commandName, translatedArgs)
     newUsage( ply, commandName )
 end ) 
 
-hook.Add( "ULibCommandCalled", "CFC_UlibCooldowns_Called")
 hook.Add( "ULibCommandCalled", "CFC_UlibCooldowns_Called", function( ply, commandName, args )
     if not canRun( ply, commandName ) then 
         ULib.tsayError( ply, "You are using this command too much", true)
