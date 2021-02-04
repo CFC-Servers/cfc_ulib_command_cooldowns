1232f5822946d202e9eb5c2bb8954991cb3dbad4
legokidlogan <legokidlogan@gmail.com>
2021-02-03 17:11:18 -0700

    Convert config to convars

--- lua/autorun/server/cfc_ulib_cooldowns.lua
+++ lua/autorun/server/cfc_ulib_cooldowns.lua
@@ -1,11 +1,20 @@
 
--- { amount = int, decayRate = int }
--- using a commands adds to its counter if this counter is greater than `amount` you can no longer run the command
+-- { amount = int, decayRate = float }
+-- Using a command adds to its counter. If this counter is greater than `amount`, you can no longer run the command
 -- 1 is subtracted from the counter every `decayRate` seconds
 local ULIB_COOLDOWNS = {
-    ["default"]       = {amount=3, decayRate=2},
-    ["ulx ragdoll"]   = {amount=2, decayRate=5},
-    ["ulx unragdoll"] = {amount=2, decayRate=5},
+    ["default"] = {
+        amount = CreateConVar( "cfc_ulib_cooldowns_default_amount", 3, FCVAR_NEVER_AS_STRING, "The max number of times most ULX commands can be used by a player within a timeframe. A value of zero means no limit. (default 3)", 0, 50000 ),
+        decayRate = CreateConVar( "cfc_ulib_cooldowns_default_decay", 2, FCVAR_NEVER_AS_STRING, "The number of seconds it takes for a command's counter to decrease by one. (default 2)", 0.001, 50000 )
+    },
+    ["ulx ragdoll"] = {
+        amount = CreateConVar( "cfc_ulib_cooldowns_ragdoll_amount", 2, FCVAR_NEVER_AS_STRING, "The max number of times ulx ragdoll can be used by a player within a timeframe. A value of zero means no limit. (default 2)", 0, 50000 ),
+        decayRate = CreateConVar( "cfc_ulib_cooldowns_ragdoll_decay", 5, FCVAR_NEVER_AS_STRING, "The number of seconds it takes for ulx ragdoll's counter to decrease by one. (default 5)", 0.001, 50000 )
+    },
+    ["ulx unragdoll"] = {
+        amount = CreateConVar( "cfc_ulib_cooldowns_unragdoll_amount", 2, FCVAR_NEVER_AS_STRING, "The max number of times ulx unragdoll can be used by a player within a timeframe. A value of zero means no limit. (default 2)", 0, 50000 ),
+        decayRate = CreateConVar( "cfc_ulib_cooldowns_unragdoll_decay", 5, FCVAR_NEVER_AS_STRING, "The number of seconds it takes for ulx unragdoll's counter to decrease by one. (default 5)", 0.001, 50000 )
+    },
 }
 
 local RANK_OVERRIDES = {
@@ -26,7 +35,7 @@ end
 local function isExempt( ply, commandName )
     local rankName = team.GetName( ply:Team() )
     if ply:IsAdmin() then return true end
-    local overrides = RANK_OVERRIDES[rankname]
+    local overrides = RANK_OVERRIDES[rankName]
 
     if overrides and overrides.exempt then return true end
     return false
@@ -51,13 +60,20 @@ local function canRun( ply, commandName )
     local conf = getCooldownConfig( ply, commandName )
     if not conf then return true end
 
+    local config = {
+        amount = conf.amount:GetInt(),
+        decayRate = conf.decayRate:GetFloat()
+    }
+
+    if config.amount == 0 then return true end
+
     local cooldown = getCooldownTable( ply, commandName )
 
     local timeSince = CurTime() - cooldown.lastRun
-    cooldown.count = cooldown.count - math.floor( timeSince / conf.decayRate)
+    cooldown.count = cooldown.count - math.floor( timeSince / config.decayRate)
     cooldown.count = math.max( 0, cooldown.count )
 
-    if cooldown.count >= conf.amount then
+    if cooldown.count >= config.amount then
         return false
     end
 
@@ -65,8 +81,6 @@ local function canRun( ply, commandName )
 end
 
 local function newUsage( ply, commandName )
-    local conf = getCooldownConfig( ply, commandName )
-    if not conf then return end
     local cooldown = getCooldownTable( ply, commandName )
 
     cooldown.lastRun = CurTime()
